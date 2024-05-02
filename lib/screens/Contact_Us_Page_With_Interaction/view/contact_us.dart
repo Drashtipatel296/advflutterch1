f8982940c19157157cccb5aec6cadd17f0c5fd17
Admin <drashtipatel7080@gmail.com>
2024-05-02 11:08:33 +0530

    02-05-2024

--- lib/screens/Contact_Us_Page_With_Interaction/view/contact_us.dart
+++ lib/screens/Contact_Us_Page_With_Interaction/view/contact_us.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
 
-// contact app
-
 class ContactApp extends StatelessWidget {
   const ContactApp({super.key});
 
@@ -34,94 +32,94 @@ class ContactApp extends StatelessWidget {
             const SizedBox(
               height: 50,
             ),
-            Container(
-              height: 70,
-              width: 320,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Icon(
-                    Icons.phone_android,
-                    color: Colors.blueAccent,
-                    size: 35,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Uri url = Uri.parse('tel: +91 9327642743');
-                      launcher.launchUrl(url);
-                    },
-                    child: const Text(
+            InkWell(
+              onTap: () {
+                Uri url = Uri.parse('tel: +91 9327642743');
+                launcher.launchUrl(url);
+              },
+              child: Container(
+                height: 70,
+                width: 320,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.phone_android,
+                      color: Colors.blueAccent,
+                      size: 35,
+                    ),
+                    Text(
                       '+91 12345 67890',
                       style: TextStyle(fontSize: 20, color: Colors.black),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             const SizedBox(
               height: 30,
             ),
-            Container(
-              height: 70,
-              width: 320,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Icon(
-                    Icons.email_outlined,
-                    color: Colors.blueAccent,
-                    size: 35,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Uri url = Uri.parse('mailto: drashtipatel7080@gmail.com');
-                      launcher.launchUrl(url);
-                    },
-                    child: const Text(
+            InkWell(
+              onTap: () {
+                Uri url = Uri.parse('mailto: drashtipatel7080@gmail.com');
+                launcher.launchUrl(url);
+              },
+              child: Container(
+                height: 70,
+                width: 320,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.email_outlined,
+                      color: Colors.blueAccent,
+                      size: 35,
+                    ),
+                    Text(
                       'info@xymail.com',
                       style: TextStyle(fontSize: 20, color: Colors.black),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             const SizedBox(
               height: 30,
             ),
-            Container(
-              height: 70,
-              width: 320,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Icon(
-                    Icons.sms_outlined,
-                    color: Colors.blueAccent,
-                    size: 35,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Uri url = Uri.parse('sms: +91 9327642743');
-                      launcher.launchUrl(url);
-                    },
-                    child: const Text(
+            InkWell(
+              onTap: () {
+                Uri url = Uri.parse('sms: +91 9327642743');
+                launcher.launchUrl(url);
+              },
+              child: Container(
+                height: 70,
+                width: 320,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.sms_outlined,
+                      color: Colors.blueAccent,
+                      size: 35,
+                    ),
+                    Text(
                       '+91 12345 67890',
                       style: TextStyle(fontSize: 20, color: Colors.black),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             const SizedBox(
@@ -151,64 +149,62 @@ class ContactApp extends StatelessWidget {
                   const SizedBox(
                     height: 10,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 35),
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/img/linkedin.jpg'),
+                  InkWell(
+                    onTap: () {
+                      Uri url = Uri.parse('https://www.linkedin.com/feed/');
+                      launcher.launchUrl(url);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 35),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/img/linkedin.jpg'),
+                              ),
                             ),
                           ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Uri url =
-                                Uri.parse('https://www.linkedin.com/feed/');
-                            launcher.launchUrl(url);
-                          },
-                          child: const Text(
+                          const Text(
                             '         Linkedin',
                             style: TextStyle(fontSize: 20, color: Colors.black),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                   const Padding(
                     padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                     child: Divider(),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 35),
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage('assets/img/github.png'),
+                  InkWell(
+                    onTap: () {
+                      Uri url = Uri.parse('https://github.com/Drashtipatel296');
+                      launcher.launchUrl(url);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 35),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/img/github.png'),
+                              ),
                             ),
                           ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Uri url =
-                                Uri.parse('https://github.com/Drashtipatel296');
-                            launcher.launchUrl(url);
-                          },
-                          child: const Text(
+                          const Text(
                             '         GitHub',
                             style: TextStyle(fontSize: 20, color: Colors.black),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ],
